d8a64591bf227b367790cf4e8806180f0c00b10a
Abby Tyrell <77738923+atyrell3@users.noreply.github.com>
2025-07-16 18:01:35 -0400

    update minimal example

--- run_reports.R
+++ run_reports.R
@@ -1,14 +1,35 @@
 rmarkdown::render(
   input = here::here("parent_report.Rmd"),
-  output_file = here::here("parent_report_midatlantic.pdf"),
-  params = list(region = "MidAtlantic"),
+  output_file = here::here("output/parent_report_midatlantic.pdf"),
+  params = list(
+    region = "MidAtlantic",
+    fig_caption = here::here("utils/fig-captions.csv")
+  ),
+  intermediates_dir = here::here("output"),
+  knit_root_dir = here::here("output"),
   output_dir = here::here("output"),
-  output_format = bookdown::pdf_document2(includes = rmarkdown::includes(in_header = "utils/header1_midatlantic.tex"))
+  output_format = # c(
+    bookdown::pdf_document2(
+      includes = rmarkdown::includes(in_header = here::here("utils/header1_midatlantic.tex")),
+      keep_tex = TRUE,
+      toc = FALSE,
+      number_sections = FALSE
+    ) # ,
+  # bookdown::html_document2(df_print = "paged")#,
+  # bookdown::word_document2(toc = FALSE)
+  # )
 )
 
 rmarkdown::render(here::here("parent_report.Rmd"),
   output_file = here::here("parent_report_newengland.pdf"),
   params = list(region = "NewEngland"),
+  intermediates_dir = here::here("output"),
+  knit_root_dir = here::here("output"),
   output_dir = here::here("output"),
-  output_format = bookdown::pdf_document2(includes = rmarkdown::includes(in_header = "utils/header1_newengland.tex"))
+  output_format = bookdown::pdf_document2(
+    includes = rmarkdown::includes(in_header = here::here("utils/header1_newengland.tex")),
+    keep_tex = TRUE,
+    toc = FALSE,
+    number_sections = FALSE
+  )
 )
