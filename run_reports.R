42e227479cf206e102324605bb0df0b69189102f
Abby Tyrell <77738923+atyrell3@users.noreply.github.com>
2025-07-29 17:11:31 -0400

    compile full mid report, mab-oa chunk is having problems

--- run_reports.R
+++ run_reports.R
@@ -1,5 +1,3 @@
-
-
 # mid report -- pdf
 
 rmarkdown::render(
@@ -9,22 +7,25 @@ rmarkdown::render(
     region = "MidAtlantic",
     fig_caption = here::here("utils/figure_captions_summary.csv"),
     ## TODO: caching saves as parent file name, could cause problems with regions
+    ## TODO: caching does not speed up report generation??
     cache = TRUE
   ),
   # intermediates_dir = here::here("output"),
   # knit_root_dir = here::here("output"),
   # output_dir = here::here("output"),
-  output_format = # c(
-    bookdown::pdf_document2(
-      includes = rmarkdown::includes(in_header = here::here("utils/header1_midatlantic.tex")),
-      keep_tex = TRUE,
-      toc = FALSE,
-      number_sections = FALSE
-    ) # ,
+  # c(
+  output_format = bookdown::pdf_document2(
+    includes = rmarkdown::includes(
+      in_header = here::here("utils/header1_midatlantic.tex")
+    ),
+    keep_tex = TRUE,
+    toc = FALSE,
+    number_sections = FALSE
+  ) # ,
   # bookdown::html_document2(df_print = "paged")#,
   # bookdown::word_document2(toc = FALSE)
-  )
 )
+# )
 
 ## mid report -- html
 
@@ -43,14 +44,17 @@ rmarkdown::render(
 
 ## NE report
 
-rmarkdown::render(here::here("parent_report.Rmd"),
+rmarkdown::render(
+  here::here("parent_report.Rmd"),
   output_file = here::here("newengland.pdf"),
   params = list(region = "NewEngland"),
   # intermediates_dir = here::here("output"),
   # knit_root_dir = here::here("output"),
   # output_dir = here::here("output"),
   output_format = bookdown::pdf_document2(
-    includes = rmarkdown::includes(in_header = here::here("utils/header1_newengland.tex")),
+    includes = rmarkdown::includes(
+      in_header = here::here("utils/header1_newengland.tex")
+    ),
     keep_tex = TRUE,
     toc = FALSE,
     number_sections = FALSE
