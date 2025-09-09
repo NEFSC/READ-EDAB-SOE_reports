8c148eb6b8bd019b1cbf287386496c1d692458b3
Abby Tyrell <77738923+atyrell3@users.noreply.github.com>
2025-09-09 18:16:53 -0400

    revise Rmds to pull figure images from csv key

--- run_reports.R
+++ run_reports.R
@@ -10,10 +10,6 @@ rmarkdown::render(
     cache = FALSE,
     id_child_docs = TRUE
   ),
-  # intermediates_dir = here::here("output"),
-  # knit_root_dir = here::here("output"),
-  # output_dir = here::here("output"),
-  # c(
   output_format = bookdown::pdf_document2(
     includes = rmarkdown::includes(
       in_header = here::here("utils/header1_midatlantic.tex")
@@ -21,11 +17,8 @@ rmarkdown::render(
     keep_tex = TRUE,
     toc = FALSE,
     number_sections = FALSE
-  ) # ,
-  # bookdown::html_document2(df_print = "paged")#,
-  # bookdown::word_document2(toc = FALSE)
+  )
 )
-# )
 difftime(Sys.time(), now)
 
 ## mid report (html) ----
@@ -51,12 +44,9 @@ rmarkdown::render(
   params = list(
     region = "NewEngland",
     fig_caption = here::here("utils/figure_captions_summary.csv"),
-    cache = TRUE,
+    cache = FALSE,
     id_child_docs = TRUE
   ),
-  # intermediates_dir = here::here("output"),
-  # knit_root_dir = here::here("output"),
-  # output_dir = here::here("output"),
   output_format = bookdown::pdf_document2(
     includes = rmarkdown::includes(
       in_header = here::here("utils/header1_newengland.tex")
