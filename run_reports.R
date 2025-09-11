1b58a710cf92c07a9561f57a607aff293f866c91
Abby Tyrell <77738923+atyrell3@users.noreply.github.com>
2025-09-11 18:25:29 -0400

    developing methods to knit both regions into a single report for text editing

--- run_reports.R
+++ run_reports.R
@@ -1,3 +1,30 @@
+## both reports, for text edits (pdf) ----
+now <- Sys.time()
+rmarkdown::render(
+  input = here::here("parent_report.Rmd"),
+  output_file = "bothregions.pdf",
+  params = list(
+    region = "MidAtlantic",
+    # TODO: have to pass a region, working on overriding in child docs and figures
+    fig_caption = here::here("utils/figure_captions_summary.csv"),
+    ## TODO: caching saves as parent file name, could cause problems with regions
+    cache = FALSE,
+    id_child_docs = TRUE,
+    draft = TRUE
+  ),
+  output_format = bookdown::pdf_document2(
+    includes = rmarkdown::includes(
+      in_header = here::here("utils/header1_midatlantic.tex")
+      # TODO: create different header for draft doc?
+    ),
+    keep_tex = TRUE,
+    toc = FALSE,
+    number_sections = FALSE
+  )
+)
+difftime(Sys.time(), now)
+
+
 ## mid report (pdf) ----
 now <- Sys.time()
 rmarkdown::render(
@@ -8,7 +35,8 @@ rmarkdown::render(
     fig_caption = here::here("utils/figure_captions_summary.csv"),
     ## TODO: caching saves as parent file name, could cause problems with regions
     cache = FALSE,
-    id_child_docs = TRUE
+    id_child_docs = TRUE,
+    draft = FALSE
   ),
   output_format = bookdown::pdf_document2(
     includes = rmarkdown::includes(
