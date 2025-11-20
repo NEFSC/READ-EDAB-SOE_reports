7f4196df894950aea088726d37938f21bbd34de0
Stephanie Owen <1626609313113305@mil>
2025-11-20 11:10:43 -0500

    run both_regions report with updated file names

--- run_reports.R
+++ run_reports.R
@@ -14,8 +14,7 @@ rmarkdown::render(
   ),
   output_format = bookdown::pdf_document2(
     includes = rmarkdown::includes(
-      in_header = here::here("utils/header1_midatlantic.tex")
-      # TODO: create different header for draft doc?
+      in_header = here::here("utils/header1_bothreports.tex")
     ),
     keep_tex = TRUE,
     toc = FALSE,
@@ -49,21 +48,6 @@ rmarkdown::render(
 )
 difftime(Sys.time(), now)
 
-## mid report (html) ----
-
-rmarkdown::render(
-  input = here::here("parent_report.Rmd"),
-  # output_file = "midatlantic.pdf",
-  output_file = "midatlantic.html",
-  params = list(
-    region = "MidAtlantic",
-    fig_caption = here::here("utils/figure_captions_summary.csv"),
-    ## TODO: caching saves as parent file name, could cause problems with regions
-    cache = TRUE
-  ),
-  output_format = bookdown::html_document2()
-)
-
 ## NE report (pdf) ----
 now <- Sys.time()
 rmarkdown::render(
@@ -85,28 +69,3 @@ rmarkdown::render(
   )
 )
 difftime(Sys.time(), now)
-
-## NE report (docx) ----
-### don't render to word like this, convert the pdf
-
-rmarkdown::render(
-  here::here("parent_report.Rmd"),
-  output_file = here::here("newengland.docx"),
-  params = list(
-    region = "NewEngland",
-    fig_caption = here::here("utils/figure_captions_summary.csv"),
-    cache = TRUE,
-    id_child_docs = TRUE
-  ),
-  # intermediates_dir = here::here("output"),
-  # knit_root_dir = here::here("output"),
-  # output_dir = here::here("output"),
-  # output_format = bookdown::word_document2(
-  #   # includes = rmarkdown::includes(
-  #   #   in_header = here::here("utils/header1_newengland.tex")
-  #   # ),
-  #   # keep_tex = TRUE,
-  #   # toc = FALSE,
-  #   # number_sections = FALSE
-  # )
-)
