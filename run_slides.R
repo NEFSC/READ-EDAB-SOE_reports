4318f35c69e0941cd94c895f6fa8f6b6c5731c3c
Abby Tyrell <77738923+atyrell3@users.noreply.github.com>
2025-12-09 18:12:08 -0500

    add new england slides

--- run_slides.R
+++ run_slides.R
@@ -13,3 +13,34 @@ quarto::quarto_render(
   output_format = "all"
 )
 difftime(Sys.time(), now)
+
+## ne slides ----
+now <- Sys.time()
+quarto::quarto_render(
+  input = here::here("parent_presentation.qmd"),
+  output_file = "newengland.html",
+  execute_params = list(
+    region = "NewEngland",
+    council = "NEFMC",
+    fig_caption = here::here("utils/figure_captions_summary.csv"),
+    date = Sys.Date() |> format("%B %d, %Y"),
+    author = "Joe Caracappa, lead editor, NEFSC"
+  ),
+  output_format = "all"
+)
+difftime(Sys.time(), now)
+
+## all slides ----
+now <- Sys.time()
+quarto::quarto_render(
+  input = here::here("parent_presentation.qmd"),
+  execute_params = list(
+    region = "Both",
+    council = "NEFMC, MAFMC",
+    fig_caption = here::here("utils/figure_captions_summary.csv"),
+    date = Sys.Date() |> format("%B %d, %Y"),
+    author = "NEFSC"
+  ),
+  output_format = "all"
+)
+difftime(Sys.time(), now)
