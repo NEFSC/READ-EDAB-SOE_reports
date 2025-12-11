b744d312b764c496be0800c72798bb89a5764916
Abby Tyrell <77738923+atyrell3@users.noreply.github.com>
2025-12-11 18:15:14 -0500

    update kobe plots

--- scripts/stock_status_kobe.R
+++ scripts/stock_status_kobe.R
@@ -173,3 +173,90 @@ ggplot2::ggsave(
   dpi = 72,
   bg = "white"
 )
+
+# try faceting ----
+
+recent_dat2 <- dat |>
+  dplyr::left_join(short_name, by = c("stock_name" = "Stock")) |>
+  dplyr::group_by(stock_name) |>
+  dplyr::mutate(max_year = max(assessment_year, na.rm = TRUE)) |>
+  dplyr::filter(assessment_year == max_year) |>
+  dplyr::ungroup() |>
+  dplyr::select(-max_year) |>
+  dplyr::mutate(
+    f_fmsy = dplyr::case_when(
+      is.na(f_fmsy) & !is.na(b_bmsy) ~ 100,
+      TRUE ~ f_fmsy
+    ),
+    Cat = dplyr::case_when(f_fmsy == 100 ~ FALSE, TRUE ~ TRUE),
+    Code = dplyr::case_when(
+      stock_name == "Atlantic salmon - Gulf of Maine" ~ "Salmon",
+      stock_name == "Atlantic wolffish - Gulf of Maine / Georges Bank" ~
+        "Wolffish",
+      TRUE ~ Code
+    )
+  )
+
+
+plt_dat <- recent_dat2 |>
+  dplyr::filter(stringr::str_detect(jurisdiction, "NEFMC"))
+
+max_f <- max(plt_dat$f_fmsy[which(plt_dat$f_fmsy < 100)], na.rm = TRUE)
+
+ybreaks <- seq(
+  0,
+  ifelse(max_f > 1, max_f, 1),
+  by = 0.25
+)
+
+plt_dat |>
+  ggplot2::ggplot() +
+  ggplot2::geom_vline(xintercept = 1, linetype = "dotted") +
+  ggplot2::geom_vline(xintercept = 0.5, linetype = "dashed") +
+  ggplot2::geom_hline(
+    ggplot2::aes(yintercept = yval),
+    linetype = "dashed",
+    data = tibble::tibble(Cat = c(TRUE, FALSE), yval = c(1, NA))
+  ) +
+  ggplot2::geom_point(
+    ggplot2::aes(
+      x = b_bmsy,
+      y = f_fmsy
+    )
+  ) +
+  ggrepel::geom_text_repel(
+    ggplot2::aes(
+      x = b_bmsy, #geom_text_repel auto-jitters text around points
+      y = f_fmsy,
+      label = Code
+    ),
+    max.overlaps = 50
+  ) +
+  ggplot2::scale_y_continuous(
+    breaks = c(ybreaks, 100),
+    labels = c(ybreaks, "No F/Fmsy\nEstimate")
+  ) +
+  ggplot2::xlab(expression(~ B / B[msy])) +
+  ggplot2::ylab(expression(~ F / F[msy])) +
+  ggplot2::scale_color_viridis_c() +
+  ecodata::theme_ts() +
+  ecodata::theme_title() +
+  ggplot2::facet_grid(
+    rows = ggplot2::vars(Cat),
+    scales = "free_y",
+    space = "free_y"
+  ) +
+  ggplot2::theme(
+    legend.position = "bottom",
+    legend.direction = "vertical",
+    strip.text = ggplot2::element_blank(),
+    strip.background = ggplot2::element_blank()
+  )
+
+ggplot2::ggsave(
+  "images/stock_assessment_bbmsy_ne.png",
+  width = 6.5,
+  height = 5,
+  units = "in",
+  dpi = 300,
+)
