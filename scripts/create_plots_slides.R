758f519cefb46b88aca61cf0b60b32f3f6218e1b
Stephanie Owen <1626609313113305@mil>
2026-01-20 14:55:36 -0500

    remove tuna and billfishes from rec_hms, update caption

--- scripts/create_plots_slides.R
+++ scripts/create_plots_slides.R
@@ -160,8 +160,8 @@ save_plot(
   plot_expression = {
     rec_hms_plot <- ecodata::plot_rec_hms(report = region, n = 10) +
       ggplot2::scale_color_discrete(
-        limits = c("LargeCoastal", "SmallCoastal", "Prohibited", "Billfishes", "Scombridae"),
-        labels = c("Large Coastal", "Small Coastal", "Prohibited", "Billfishes", "Scombridae")
+        limits = c("LargeCoastal", "SmallCoastal", "Prohibited"),
+        labels = c("Large Coastal", "Small Coastal", "Prohibited")
       ) +
       ggplot2::ggtitle(paste(
         region2,
